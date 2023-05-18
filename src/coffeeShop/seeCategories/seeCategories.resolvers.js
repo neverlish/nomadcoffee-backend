ed819650e2b5e13409a9137a191feb123356897b
Jinho Hyeon <neverlish@gmail.com>
2023-05-18 21:58:56 +0900

    fix typo

--- src/coffeeShop/seeCategories/seeCategories.resolvers.js
+++ src/coffeeShop/seeCategories/seeCategories.resolvers.js
@@ -2,7 +2,7 @@ import client from "../../client";
 
 export default {
   Query: {
-    seeCoffeeShop: (_, { id, page }) => 
+    seeCategories: (_, { id, page }) => 
       client.category.findMany({
         take: 5,
         skip: (page - 1) * 5,
